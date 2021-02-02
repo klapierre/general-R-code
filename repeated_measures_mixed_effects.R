5fe3cf9f4db81ed92c1bc2d76e6074c7b271a4d1
klapierre <lapierre.kimberly@gmail.com>
2021-02-02 16:11:39 -0700

    repeated measures

---
+++ repeated_measures_mixed_effects.R
@@ -0,0 +1,24 @@
+library(nlme)
+library(lsmeans)
+library(car)
+library(performance)
+
+#model -- repeated measures with autoregressive structure
+summary(heightModel <- lme(height ~ diversity*warming*year,
+                           data=growthData, 
+                           random=~1|block,
+                           correlation=corAR1(form=~year|block/plant), #autoregressive structure
+                           control=lmeControl(returnObject=T)))
+check_model(heightModel)
+Anova(heightModel, type=3) #this may not work, use shuffling function to calculate type 3 effects instead
+
+
+
+#model -- repeated measures with compound symmetry structure
+summary(heightModel <- lme(height ~ diversity*warming*year,
+                           data=growthData, 
+                           random=~1|block,
+                           correlation=corCompSymm(form=~1|block/plant), #autoregressive structure
+                           control=lmeControl(returnObject=T)))
+check_model(heightModel)
+Anova(heightModel, type=3) #this may not work, use shuffling function to calculate type 3 effects instead
